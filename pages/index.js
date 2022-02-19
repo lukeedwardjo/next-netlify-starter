808b0b2791a7a5c4046f7d3ee329b8bbe12a2495
Luke Johnson <lukejohnson@Lukes-MacBook-Air.local>
2022-02-19 14:29:39 -0600

    change content

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Home | Luke Johnson</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Luke Johnson" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          software engineer
         </p>
       </main>
 
